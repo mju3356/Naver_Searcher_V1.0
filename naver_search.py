eb6aeae1e509fc8190b88abb63bc2e9312924206
M <whddnr3356@gmail.com>
2020-09-02 21:16:20 +0900

    Develoment Porgram 60% 1.Adding a Notepad Storage Function as a Result of Extraction 2.(Updating) Only post function is available

--- naver_search.py
+++ naver_search.py
@@ -18,13 +18,11 @@ class TestForm(QMainWindow, Ui_MainWindow):
         self.init_setting()
         self.init_signal()
 
-
-
     def init_setting(self):
         self.input_search.setFocus(True)
         # 쓰레드 작업으로 시간 계속 업데이트 필요
         self.Show_StatusMsg(str(datetime.datetime.now()))
-        self.Url=None
+        self.Url = None
 
     def init_signal(self):
         self.search_btn.clicked.connect(self.Start_Searching)
@@ -39,56 +37,107 @@ class TestForm(QMainWindow, Ui_MainWindow):
         self.statusbar.showMessage(msg)
 
     def Start_Searching(self):
-        KeyWord = self.input_search.text().strip()
-        self.Url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=' + KeyWord
-        if KeyWord is None or KeyWord == '' or not KeyWord:
+        self.KeyWord = self.input_search.text().strip()
+        self.Url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=' + self.KeyWord
+        if self.KeyWord is None or self.KeyWord == '' or not self.KeyWord:
             QMessageBox.about(self, "Error", '검색어를 입력하세요')
             self.input_search.setFocus(True)
             return None
         self.webEngineView.load(QUrl(self.Url))
-        self.search_his.addItem(KeyWord)
-
-        for i in soup.select('div._prs_nws_all dt'):
-            print(i.text)
+        self.search_his.addItem(self.KeyWord)
 
     def Delete_History(self):
         self.search_his.clear()
 
     def History_Extraction(self):
-        #Ssd=Save_Setting_Dialog
-        Ssd=Ui_Save_Setting_Dialog()
+        # Ssd=Save_Setting_Dialog
+        Ssd = Ui_Save_Setting_Dialog()
         Ssd.exec_()
-        self.Check_Post=Ssd.Check_Post
-        self.Check_Q=Ssd.Check_Q
-        self.Check_Cafe=Ssd.Check_Cafe
-        self.Check_News=Ssd.Check_News
-        self.Check_Wiki=Ssd.Check_Wiki
-        self.Check_Company=Ssd.Check_Company
-        self.Check_Image=Ssd.Check_Image
-        self.Check_Pinfo=Ssd.Check_PInfo
-        self.Check_Video=Ssd.Check_Video
-        self.Check_Website=Ssd.Check_Website
-        self.Check_Post=Ssd.Check_Post
-        self.Accept=Ssd.Accept
+        self.Check_Post = Ssd.Check_Post
+        self.Check_Q = Ssd.Check_Q
+        self.Check_Cafe = Ssd.Check_Cafe
+        self.Check_News = Ssd.Check_News
+        self.Check_Wiki = Ssd.Check_Wiki
+        self.Check_Company = Ssd.Check_Company
+        self.Check_Image = Ssd.Check_Image
+        self.Check_Pinfo = Ssd.Check_PInfo
+        self.Check_Video = Ssd.Check_Video
+        self.Check_Website = Ssd.Check_Website
+        self.Check_Post = Ssd.Check_Post
+        self.Accept = Ssd.Accept
 
         if not self.Accept:
             return None
 
         if not self.Url:
-            QMessageBox.about(self,"Error",'검색어를 입력하세요')
+            QMessageBox.about(self, "Error", '검색어를 입력하세요')
             return None
 
-        req = requests.get(self.Url)
-        WebData = BeautifulSoup(req.content, 'html.parser')
+        with open(self.KeyWord + '검색결과.txt', 'w') as Np:
+            pass
+
+        if Ssd.Check_Post.isChecked:
+            self.Crawling_Data(
+                Url='https://m.post.naver.com/search/post.nhn?keyword=',
+                Select='strong.tit_feed',
+                DataName='포스트'
+            )
 
 
-
-
-
-
-
-
-
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Wiki.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='ul.lst_krdic br',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
+        if Ssd.Check_Q.isChecked:
+            self.Crawling_Data(
+                Url='https://search.naver.com/search.naver?where=kin&sm=tab_jum&query=',
+                Select='dt.question > a',
+                DataName='지식인'
+            )
 
 
     def SetForcus_Item(self):
@@ -98,7 +147,20 @@ class TestForm(QMainWindow, Ui_MainWindow):
     def DoubleClick_Item(self):
         self.input_search.setText(self.search_his.currentItem().text().strip())
         self.Start_Searching()
-        item = self.search_his.takeItem(self.search_his.count()-1)
+        item = self.search_his.takeItem(self.search_his.count() - 1)
+
+    def Crawling_Data(self,Url,Select,DataName):
+        req = requests.get(Url + self.KeyWord)
+        WebData = BeautifulSoup(req.content, 'html.parser')
+        res = WebData.select(Select)
+
+        with open(self.KeyWord+'검색결과.txt','a') as Np:
+            Np.write(DataName+'n')
+            if res == []:
+                Np.write('검색결과가 없습니다.\n')
+            for i in res:
+                Np.write('\t' + i.text.strip() + '\n')
+            Np.write('--------------------------------------------------------------------------------------------------------------------------------------')
 
 
 if __name__ == "__main__":
